59d0604d1b847df57eb8b99171db6033ae71bc5e
Satyam Sharma <55931484+codedestructed007@users.noreply.github.com>
2024-02-12 13:18:48 +0530

    Recursion

---
+++ Number_of_ways_the_summation_can_be_N.py
@@ -0,0 +1,24 @@
+"""
+Given 3 numbers {1, 3, 5}, the task is to tell the total number of ways we can
+form a number N using the sum of the given three numbers.
+(allowing repetitions and different arrangements)
+"""
+
+# Using Recursion
+# Time com
+
+class Solution():
+    def possible_ways(self,n):
+
+        # base condition
+        if n == 0:
+            return 1
+
+        if n < 0 :
+            return 0
+
+        return self.possible_ways(n-1) + self.possible_ways(n-3) + self.possible_ways(n-5)
+
+
+
+    
